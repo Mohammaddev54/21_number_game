38916cd73ef3942727ea18768bf3c2abe5fc2e59
Mohammaddev54 <mohammaddev54@gmail.com>
2025-07-13 16:36:26 -0500

    Update 21_number.py

--- 21_number.py
+++ 21_number.py
@@ -1,3 +1,20 @@
+"""
+THIS SCRIPT IS ONLY MEANT TO BE RUN ON WINDOWS.
+
+21, Bagram, or Twenty plus one is a game which progresses by counting up 1 to 21, 
+with the player who calls "21" is eliminated. It can be played between any number of players. 
+Implementation This is a simple 21 number game using Python programming language. 
+The game illustrated here is between the player and the computer. 
+There can be many variations in the game.
+
+The player can choose to start first or second.
+The list of numbers is shown before the Player takes his turn so that it becomes convenient.
+If consecutive numbers are not given in input then the player is automatically disqualified.
+The player loses if he gets the chance to call 21 and wins otherwise.
+Winning against the computer can be possible by choosing to play second. 
+The strategy is to call numbers till the multiple of 4 which would eventually 
+lead to 21 on computer hence making the player the winner. 
+"""
 import os
 
 
@@ -73,12 +90,12 @@ def computer_choice_of_numbers(number_list):
     print(number_list)
 
 
-def verify_user_input(user_input, ):
-    if user_input == 21:
-        #LOSING DUE TO user_input==21
+def verify_user_input(user_input):
+    if user_input == 0 or user_input == 21:
+        #LOSING DUE TO user_input==21 OR user_input==0
         return False
-    elif user_input <= 0 or user_input > 21:
-        # LOSING DUE TO user_input<=0 or user_input>21
+    elif user_input < 0 or user_input > 21:
+        # LOSING DUE TO user_input<0 or user_input>21
         return False
     return True
 
